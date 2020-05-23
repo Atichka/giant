41f37855e8ea032e85b6d3b0e3856bc5e0f698c9
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-05-24 00:57:20 +0300

    add adaptation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 
 import Swiper from 'swiper';
+const buttonBuy = document.querySelector('.card__button');
+const buttonPrice = document.querySelector('.card__price');
 
 const swiper = new Swiper('.swiper-container', {
   direction: 'horizontal',
@@ -20,3 +22,32 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-scrollbar',
   },
 })
+
+var reply_click = function()
+{
+  event.target.classList.toggle('gradient');
+}
+document.getElementById('0').onclick = reply_click;
+document.getElementById('1').onclick = reply_click;
+document.getElementById('2').onclick = reply_click;
+document.getElementById('3').onclick = reply_click;
+
+// buttonBuy.addEventListener('click', function() {
+//   alert(buttonBuy.id);
+  // event.target.classList.toggle('gradient');
+// });
+
+// window.onload = function () {
+  // var buttonBuy = document.getElementsByTagName("card__button");
+  // for(var i = 0; i < buttonBuy.length; i++) {
+  // buttonBuy[i].onclick = function () {
+    // event.target.classList.toggle('gradient');
+        //  }
+        // }
+// })
+
+// window.onload = function () {
+//   for(let i = 0; i < buttonBuy.length; i++) {
+//     buttonBuy.se = i;
+//   }
+// }
